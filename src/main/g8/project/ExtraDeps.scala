a97e50e93aedd0382c2e9db927d07bb92f542244
Marco Yuen <marcoy@gmail.com>
2020-10-25 21:40:07 -0400

    Bump sbt to 1.4.1 and add more extra deps

--- src/main/g8/project/ExtraDeps.scala
+++ src/main/g8/project/ExtraDeps.scala
@@ -2,12 +2,83 @@ import sbt._
 
 object ExtraDeps {
   object Versions {
-    val sttp = "2.2.9"
+    object commons {
+      val codec   = "1.15"
+      val crypto  = "1.1.0"
+      val exec    = "1.3"
+      val io      = "2.8.0"
+      val logging = "1.2"
+      val net     = "3.7.2"
+      val vfs     = "2.6.0"
+    }
+
+    val jsch            = "0.1.55"
+    val scalacheckFaker = "5.0.10"
+    val selenium        = "3.141.59"
+    val sshj            = "0.30.0"
+    val sttp            = "2.2.9"
+    val zioAws          = "2.14.7.0"
   }
 
+  val jsch: Seq[ModuleID] = Seq(
+    "com.jcraft" % "jsch" % Versions.jsch
+  )
+
+  val scalacheckFaker: Seq[ModuleID] = Seq(
+    "io.github.etspaceman" %% "scalacheck-faker" % Versions.scalacheckFaker
+  )
+
+  val selenium: Seq[ModuleID] = Seq(
+    "org.seleniumhq.selenium" % "selenium-java"          % Versions.selenium,
+    "org.seleniumhq.selenium" % "selenium-chrome-driver" % Versions.selenium
+  )
+
+  val sshj = Seq(
+    "com.hierynomus" % "sshj" % Versions.sshj
+  )
+
   val sttp: Seq[ModuleID] = Seq(
-    "com.softwaremill.sttp.client" %% "core"                          % Versions.sttp
-  , "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % Versions.sttp
-  , "com.softwaremill.sttp.client" %% "slf4j-backend"                 % Versions.sttp
+    "com.softwaremill.sttp.client" %% "core"                          % Versions.sttp,
+    "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % Versions.sttp,
+    "com.softwaremill.sttp.client" %% "slf4j-backend"                 % Versions.sttp
+  )
+
+  val zioAws: Seq[ModuleID] = Seq(
+    "io.github.vigoo" %% "zio-aws-eks"       % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-http4s"    % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-netty"     % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-kinesis"   % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-s3"        % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-s3control" % Versions.zioAws,
+    "io.github.vigoo" %% "zio-aws-sqs"       % Versions.zioAws
+  )
+
+  val commonsCodec: Seq[ModuleID] = Seq(
+    "commons-codec" % "commons-codec" % Versions.commons.codec
+  )
+
+  val commonsCrypto: Seq[ModuleID] = Seq(
+    "org.apache.commons" % "commons-crypto" % Versions.commons.crypto
+  )
+
+  val commonsExec: Seq[ModuleID] = Seq(
+    "org.apache.commons" % "commons-exec" % Versions.commons.exec
+  )
+
+  val commonsIO: Seq[ModuleID] = Seq(
+    "commons-io" % "commons-io" % Versions.commons.io
+  )
+
+  val commonsLogging: Seq[ModuleID] = Seq(
+    "commons-logging" % "commons-logging" % Versions.commons.logging
+  )
+
+  val commonsNet: Seq[ModuleID] = Seq(
+    "commons-net" % "commons-net" % Versions.commons.net
+  )
+
+  val commonsVfs2: Seq[ModuleID] = Seq(
+    "org.apache.commons" % "commons-vfs2"          % Versions.commons.vfs,
+    "org.apache.commons" % "commons-vfs2-examples" % Versions.commons.vfs
   )
 }

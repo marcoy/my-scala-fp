e62005d3e1b17cfc4c544c359017b2130187bdf3
Marco Yuen <marco@troops.ai>
2020-12-07 09:52:23 -0500

    Update main

--- src/main/g8/src/main/scala/$organization__packaged$/Main.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/Main.scala
@@ -1,9 +1,23 @@
-package $organization$
+package io.marcoy
 
 import zio._
-import zio.console._
+import zio.logging._
+import zio.logging.slf4j.Slf4jLogger
 
-object Main extends App {
+object Main extends zio.App {
+  object Layers {
+    def apply() = {
+      val logger = Slf4jLogger.makeWithAnnotationsAsMdc(List(LogAnnotation.CorrelationId))
+
+      logger
+    }
+  }
+
+  val program = for {
+    _ <- log.info(s"Hello World")
+  } yield ExitCode.success
+
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] =
-    putStrLn("Hello World").exitCode
+    program
+      .provideCustomLayer(Layers())
 }
